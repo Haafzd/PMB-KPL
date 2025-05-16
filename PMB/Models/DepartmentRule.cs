2f9ed5ebc6f71b50d7315906b65b58d1d2470807
Haafzd <haafizd.alhabib@gmail.com>
2025-05-16 18:56:16 +0700

    Add Class Department and DepartmentServices

---
+++ PMB/Models/DepartmentRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMB.Models
+{
+
+    public class DepartmentRule
+    {
+        public string DepartmentId { get; set; }
+        public int MinMathScore { get; set; }
+        public string RequiredSchoolOrigin { get; set; }
+    }
+}
