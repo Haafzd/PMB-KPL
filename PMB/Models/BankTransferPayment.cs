41d129162c3527c3ad061f102aa515cc63b39f09
hafidzmusyafa <hafidzmusyafa@student.telkomuniversity.ac.id>
2025-05-18 03:30:48 +0700

    deploy payment

---
+++ PMB/Models/BankTransferPayment.cs
@@ -0,0 +1,33 @@
+﻿using PMB.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMB.Models
+{
+    public class BankTransferPayment : IPaymentMethod
+    {
+        public string BankAccount { get; }
+
+        public BankTransferPayment(string bankAccount)
+        {
+            if (string.IsNullOrWhiteSpace(bankAccount))
+                throw new ArgumentException("Bank tidak bisa kosong.");
+
+            BankAccount = bankAccount;
+        }
+
+        public bool ProcessPayment(decimal amount)
+        {
+            PaymentValidator.ValidateAmount(amount);
+
+            // Simulasi proses transfer bank
+            Console.WriteLine($"Sedang memproses kepada Bank dengan besaran {amount:C} kepada akun {BankAccount}.");
+            return true; // misal pembayaran berhasil
+        }
+    }
+
+
+}
