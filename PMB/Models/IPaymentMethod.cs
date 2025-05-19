1825fe5d0f594f4d3c8b18d3b25a58bd235cf5b2
hafidzmusyafa <hafidzmusyafa@student.telkomuniversity.ac.id>
2025-05-20 01:11:59 +0700

    fixing payment

---
+++ PMB/Models/IPaymentMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMB.Models
+{
+    public interface IPaymentMethod
+    {
+        bool ProcessPayment(decimal amount);
+    }
+
+}
