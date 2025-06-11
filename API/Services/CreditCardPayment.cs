ed2846c789252d5f7c0e3bcfff5a6b7e557d8825
hafidzmusyafa <hafidzmusyafa@student.telkomuniversity.ac.id>
2025-06-11 20:35:12 +0700

    Menambahkan design pattern singleton

---
+++ API/Services/CreditCardPayment.cs
@@ -0,0 +1,36 @@
+﻿using Lib.Utils;
+
+namespace API.Services
+{
+    public class CreditCardPayment : IPaymentMethod
+    {
+        public string CardNumber { get; }
+
+        public CreditCardPayment(string cardNumber)
+        {
+            if (!PaymentValidator.IsValidPaymentFormat(cardNumber))
+                throw new ArgumentException("format kredit card salah.");
+
+            CardNumber = cardNumber;
+        }
+
+        public bool ProcessPayment(decimal amount)
+        {
+            PaymentValidator.ValidateAmount(amount);
+
+            // Simulasi proses pembayaran dengan kartu kredit
+            Console.WriteLine($"Pembayaran dengan nominal  {amount:C} menggunakan kartu ini" +
+                $" {CardNumber}.");
+            return true; // misal pembayaran berhasil
+        }
+
+        public async Task<bool> ProcessPaymentAsync(string cardNumber, decimal amount)
+        {
+            await Task.Delay(1000); // Simulasi delay
+            var paymentProcessor = new PaymentProcessor<IPaymentMethod>(this);
+            var paymentExecute = paymentProcessor.ExecutePayment(amount);
+
+            return true;
+        }
+    }
+}
---
+++ Lib/Utils/PaymentValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace Lib.Utils
+{
+    public static class PaymentValidator
+    {
+        public static void ValidateAmount(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("harus lebih besar dari 0.");
+        }
+
+        public static bool IsValidPaymentFormat(string paymentNumber)
+        {
+            return paymentNumber != null && paymentNumber.Length == 16 && paymentNumber.All(char.IsDigit);
+        }
+    }
+}
